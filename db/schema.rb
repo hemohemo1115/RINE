d8aadaab3e5499ae7c3088fc76aa196c6ac85a43
hemohemo1115 <worldtrigger728@gmail.com>
2019-12-28 08:01:39 -0800

    make logout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_28_150205) do
+ActiveRecord::Schema.define(version: 2019_12_28_155104) do
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_12_28_150205) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
